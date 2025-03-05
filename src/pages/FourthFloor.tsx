cc4f079f473b87867e902a66cc9eadb560bc3c3b
ichize <65010577+ichize@users.noreply.github.com>
2025-03-05 16:01:30 +0800

    Update FourthFloor.tsx

--- src/pages/FourthFloor.tsx
+++ src/pages/FourthFloor.tsx
@@ -26,7 +26,7 @@ const FourthFloor = () => {
                 title="Studio 4A/B"
                 description="Max Pax =28. Projector"
                 position="right"
-                modelPosition={[25, 16, -5]} 
+                modelPosition={[25, 16, 0]} 
               />
               <HoverDetails
                 title="Surau L"
